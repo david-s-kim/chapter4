0ca1ad728066cc54038ad0b966b78c1e325e79b5
david-s-kim <mycodingtool9@gmail.com>
2023-04-08 16:22:37 +0900

    응급 의료정보 앱

--- app/src/main/java/com/example/chapter4/MainActivity.kt
+++ app/src/main/java/com/example/chapter4/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.chapter4
 
+import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.view.isVisible
 import com.example.chapter4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,9 +19,59 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.goInputActivityButton.setOnClickListener {
+            // 명시적 인텐트
             val intent = Intent(this, EditActivity::class.java)
-            intent.putExtra("intentMessage", "응급의료정보")
             startActivity(intent)
         }
+
+        binding.deleteButton.setOnClickListener {
+            deleteData()
+        }
+
+        binding.emergencyContactTextView.setOnClickListener {
+            // 암시적인텐트
+            with(Intent(Intent.ACTION_VIEW)){
+                val phoneNumber = binding.emergencyContactValueTextView.text.toString()
+                    .replace("-","")
+                data = Uri.parse("tel:$phoneNumber")
+                startActivity(this)
+            }
+
+        }
+    }
+
+    // 다시 Activity 로 돌아왔을 때 onResume 으로 해당데이터가 콜백됨
+    override fun onResume() {
+        super.onResume()
+        getDataUiUpdate()
+
+    }
+
+    // 매번 userInformation 을 받아오기가 귀찮으니 static 변수를 파일(const)에 선언할수 있다.
+    private fun getDataUiUpdate() {
+        with(getSharedPreferences(USER_INFORMATION, Context.MODE_PRIVATE)) {
+            binding.nameValueTextView.text = getString(NAME, "미정")
+            binding.birthdateValueTextView.text = getString(BIRTHDATE, "미정")
+            binding.bloodTypeTextView.text = getString(BLOOD_TYPE, "미정")
+            binding.emergencyContactValueTextView.text = getString(EMERGENCY_CONTACT, "미정")
+
+            val warning = getString(WARNING, "미정")
+
+            binding.warningTextView.isVisible = warning.isNullOrEmpty().not()
+            binding.warningValueTextView.isVisible = warning.isNullOrEmpty().not()
+            if (!warning.isNullOrEmpty()) {
+                binding.warningValueTextView.text = warning
+            }
+        }
+    }
+
+    // editing 을 한 후 apply() 를 꼭 해줄것
+    private fun deleteData() {
+        with(getSharedPreferences(USER_INFORMATION, MODE_PRIVATE).edit()) {
+            clear()
+            apply()
+            getDataUiUpdate()
+        }
+        Toast.makeText(this, "초기화를 완료했습니다.", Toast.LENGTH_SHORT).show()
     }
 }
